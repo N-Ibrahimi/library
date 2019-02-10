5eb7a620b5f67822dab57a4a627b69d7fb5e8410
n-ibrahimi <nasim.hamdard21@gmail.com>
2019-02-10 14:03:41 +0100

    before controller g

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#index', as: 'home'
+  get 'contact', to: 'static_pages#contact', as: 'contact'
+  get 'team', to: 'static_pages#team', as: 'team'
+  get 'login', to: 'static_pages#login', as: 'login'
+  get 'welcome/:id', to: 'static_pages#welcome', as: 'welcome'
+
+
+  resources :users do
+    resources :reservations
+  end
+
+  resources :users do
+    resources :loans 
+  end
+
+  resources :events do
+    resources :comments
+  end
+  
+  resources :cities, only: [:index, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :books 
+  resources :events 
+  
 end
